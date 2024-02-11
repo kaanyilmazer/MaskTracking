a33f524979a9875eef241150eed6f6c63f962d25
kaan <iamyilmazer@gmail.com>
2024-02-12 00:17:46 +0300

    Sorgu Yapıldı

--- WorkSpace/Program.cs
+++ WorkSpace/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+Person person = new Person();
+person.Ad = "Kaan".ToUpper();
+person.Soyad = "Yılmazer".ToUpper();
+person.TcKimlikNo = "11111111111";
+person.DogumTarihi = new DateTime(2002, 05, 24);
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person);
+
+
