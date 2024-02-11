39d65635875fd98973762b8c7caf20d11222fe95
kaan <iamyilmazer@gmail.com>
2024-02-12 00:16:40 +0300

    Entity Eklendi

---
+++ Entities/Concrete/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Person
+    {
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+
+        public string TcKimlikNo { get; set; }
+
+        public DateTime DogumTarihi { get; set; }
+
+    }
+}
