b2a8854d8718b453ede1657ab452507e00e3fc25
kaan <iamyilmazer@gmail.com>
2024-02-12 00:16:57 +0300

    Interfaceler Eklendi

---
+++ Business/Abstract/IApplicantService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IApplicantService
+    {
+        void ApplyForMask(Person person);
+         List<Person> GetAll();
+        Task<bool> CheckPerson(Person person);
+       
+    }
+}
